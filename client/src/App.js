d4e25a8030504fbc1a5477c56fda69792e4410f2
genbigbadsmoke <the_boogey@outlook.com>
2023-09-25 12:40:48 +0100

    We Move

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-function App() {
+/** root routes */
+const router = createBrowserRouter([
+    {
+        path : '/',
+        element : <div>Root Route</div>
+    },
+    {
+        path : '/register',
+        element : <div>Register Route</div>
+    },
+])
+
+export default function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <main>
+        <RouterProvider router={router}></RouterProvider>
+    </main>
+  )
 }
-
-export default App;
